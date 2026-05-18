2d777d9f9573cae07732c5a43cdf736eef271eaa
Kishanaa <Kishanshrivastav18@gmail.com>
2026-05-18 18:50:13 +0530

    18/05/2026

--- src/PatternPrintingProblem/NumberPyramid.java
+++ src/PatternPrintingProblem/NumberPyramid.java
@@ -2,6 +2,7 @@ package PatternPrintingProblem;
 
 public class NumberPyramid {
     static void main() {
+
         int n = 5;
         for (int i = 1; i < n; i++){
             for (int j = 1; j < n-i; j++){
