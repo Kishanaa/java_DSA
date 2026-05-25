9818fe9c8f020fad854c0c6be0c304680814424a
Kishanaa <Kishanshrivastav18@gmail.com>
2026-05-25 23:28:42 +0530

    25/05/2026

---
+++ src/Array/FindElement.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.Scanner;
+
+public class FindElement {
+    static void main() {
+        int[] arr = {5,6,2,3,4,8,1,2};
+        System.out.println("Enter number to find: ");
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 0; i < arr.length; i++){
+            if (num == arr[i]){
+                System.out.println("Element found at "+i+" index");
+            }
+        }
+    }
+}
