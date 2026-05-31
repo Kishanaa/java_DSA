33c6469b3b2eb355b4c75e71050e1d4107602459
Kishanaa <Kishanshrivastav18@gmail.com>
2026-06-01 00:46:38 +0530

    01/06/2026

---
+++ src/Array/FindFirstRepeated.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.HashMap;
+/*
+    Q) Find First Repeated element in given array.
+    Time Complexity  : O(n)
+    Space Complexity : O(n)
+    Difficulty level : Easy
+ */
+public class FindFirstRepeated {
+    private static int firRep(int[] arr){
+        HashMap<Integer,Integer> rep = new HashMap<>();
+        for (int i : arr){
+            rep.put(i, rep.getOrDefault(i,0)+1);
+        }
+        for (int i = 0; i < arr.length; i++){
+            int temp = arr[i];
+            if (rep.get(temp)>1){
+                return temp;
+            }
+        }
+        return -1;
+    }
+    static void main() {
+        int[] arr = {5,1,4,2,4,7,2};
+        System.out.println(firRep(arr));
+    }
+}
