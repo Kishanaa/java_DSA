2aa9c7dbe65894d5607239af09882c24d9325b96
Kishanaa <Kishanshrivastav18@gmail.com>
2026-06-22 23:14:06 +0530

    22/06/2026

---
+++ src/leetCode/FibonacciNumber509.java
@@ -0,0 +1,18 @@
+package leetCode;
+
+public class FibonacciNumber509 {
+    static int fib(int n){
+
+        if (n == 1){
+            return 1;
+        }
+        if (n == 0){
+            return 0;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+    static void main() {
+        int num = 4;
+        System.out.println(fib(num));
+    }
+}
