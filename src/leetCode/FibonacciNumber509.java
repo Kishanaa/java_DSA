84abab915eb55f3e45e5942a40976ac40213c703
Kishanaa <Kishanshrivastav18@gmail.com>
2026-06-29 23:16:34 +0530

    29/06/2026

--- src/leetCode/FibonacciNumber509.java
+++ src/leetCode/FibonacciNumber509.java
@@ -12,7 +12,7 @@ public class FibonacciNumber509 {
         return fib(n - 1) + fib(n - 2);
     }
     static void main() {
-        int num = 5;
+        int num = 6;
         System.out.println(fib(num));
     }
 }
