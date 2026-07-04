1e6d1e1d285b601ad34a92816fd36158fe7bd7fb
Kishanaa <Kishanshrivastav18@gmail.com>
2026-07-04 23:17:50 +0530

    4/07/2026

---
+++ src/recursion/CoinChange2.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class CoinChange2 {
+    static int change(int amount, int[] coins) {
+
+        return 0;
+    }
+    static void main() {
+        int[] arr = {1,2,5};
+        int target = 5;
+
+    }
+}
