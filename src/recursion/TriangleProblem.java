4bab4844328bc10bef1c04d9e019d184597f22a4
Kishanaa <Kishanshrivastav18@gmail.com>
2026-07-12 23:03:42 +0530

    12/07/2026

--- src/recursion/TriangleProblem.java
+++ src/recursion/TriangleProblem.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TriangleProblem {
-    static int minimumTotal(List<List<Integer>> triangle, int sum, int i) {
+    static int minimumTotal(List<List<Integer>> triangle, int sum, int r, int c) {
 
-        if (i >= triangle.size()){
+        if (r >= triangle.size()){
             return sum;
         }
 
-        int include1 = minimumTotal(triangle, sum + triangle.get())
+        int ans = Integer.MAX_VALUE;
+        int include1 = minimumTotal(triangle, sum + triangle.get(r).get(c), r+1, c);
+        ans = Math.min(ans, include1);
+        int include2 = minimumTotal(triangle, sum, r+1, c+1);
+        ans = Math.min(ans, include2);
 
-        return 0;
+
+        return ans;
 
     }
     static void main() {
@@ -22,24 +27,24 @@ public class TriangleProblem {
         triangle.add(row1);
 
         List<Integer> row2 = new ArrayList<>();
-        row1.add(3);
-        row1.add(4);
+        row2.add(3);
+        row2.add(4);
         triangle.add(row2);
 
         List<Integer> row3 = new ArrayList<>();
-        row1.add(6);
-        row1.add(5);
-        row1.add(7);
+        row3.add(6);
+        row3.add(5);
+        row3.add(7);
         triangle.add(row3);
 
         List<Integer> row4 = new ArrayList<>();
-        row1.add(4);
-        row1.add(1);
-        row1.add(8);
-        row1.add(3);
+        row4.add(4);
+        row4.add(1);
+        row4.add(8);
+        row4.add(3);
         triangle.add(row4);
 
-        minimumTotal(triangle,0, 0);
+        System.out.println(minimumTotal(triangle,0, 0,0));
 
     }
 }
